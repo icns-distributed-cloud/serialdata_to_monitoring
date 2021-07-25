3e1c1ff7995d730b434bcf4c0a9b320b1842807c
kidae92 <70564639+kidae92@users.noreply.github.com>
2021-07-25 18:54:41 +0900

    Add files via upload

--- web & login with mongodb/app.py
+++ web & login with mongodb/app.py
@@ -5,8 +5,13 @@ import json
 from pymongo import MongoClient
 
 
+# 토큰
 import jwt
+
+# 토큰 만료시간을 위함
 import datetime
+
+# 비밀번호 DB저장시 암호화
 import hashlib
 
 
@@ -16,11 +21,13 @@ client = MongoClient('localhost', 27017)
 
 db = client.project
 
-SECRET_KEY = 'sparta'
+SECRET_KEY = 'iloveicns'
 
 app.config['SECRET_KEY'] = 'BCODE_Flask'
 
-
+######################
+##  HTML을 주는 부분  ##
+######################
 
 @app.route('/', methods=["GET", "POST"])
 def main():
@@ -29,6 +36,11 @@ def main():
 @app.route('/Login')
 def Login():
     return render_template('Login.html')
+
+
+# [회원가입 API]
+# id, pw을 받아서, mongoDB에 저장
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장
 
 @app.route('/api/signup', methods=['POST'])
 def make_sign():
@@ -41,6 +53,7 @@ def make_sign():
         'ID': id_receive,
         'Password': pw_hash,
     }
+    # 중복 id 유무 검사
     overlap = db.infos.count_documents({'ID': id_receive})
 
     if overlap == 0:
@@ -51,7 +64,8 @@ def make_sign():
 
 
 
-# 로그인 API
+# [로그인 API]
+# id, pw를 받아서 조회 후, 토큰 발급
 
 @app.route('/api/Login', methods=['POST'])
 def api_login():
@@ -60,14 +74,14 @@ def api_login():
 
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
 
-    # id, 암호화된 pw을 가지고 해당 유저를 찾습니다.
+    # id, 암호화된 pw을 가지고 해당 유저 검색
     check = db.infos.find_one({'ID': id_receive, 'Password': pw_hash})
 
-    # 찾으면 JWT 토큰을 만들어 발급합니다.
+    # 찾으면 JWT 토큰을 만들어 발급
     if check is not None:
         # JWT 토큰에는, payload와 시크릿키가 필요
         # 시크릿키가 있어야 토큰을 디코딩(=풀기) 해서 payload 값을 볼 수 있음
-        # id와 exp를 담고 JWT 토큰을 풀면 유저ID 값을 알 수 있습니다.
+        # id와 exp를 담고 JWT 토큰을 풀면 유저ID 값을 알 수 있음
         # exp에는 만료시간. 만료시간이 지나면, 시크릿키로 토큰을 풀 때 만료되었다고 에러가 남
         payload = {
             'ID': id_receive,
@@ -75,7 +89,7 @@ def api_login():
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
 
-        # token을 줍니다.
+        # token을 줌
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
@@ -105,29 +119,29 @@ def api_valid():
 
 
 
+# [데이터 전송 API]
 
+@app.route('/test', methods=["GET"])
+def update():
+    def generator():
+        while True:
+            conn = pymysql.connect(
+                host="163.180.117.37",
+                port=3306,
+                user="testdata",
+                passwd="iloveicns",
+                db="testdata")
 
-# @app.route('/test', methods=["GET"])
-# def update():
-#     def generator():
-#         while True:
-#             conn = pymysql.connect(
-#                 host="163.180.117.37",
-#                 port=3306,
-#                 user="testdata",
-#                 passwd="iloveicns",
-#                 db="testdata")
-#
-#             cursor = conn.cursor()
-#             query = ("select * from sensor_data order by date desc limit 1")
-#             cursor.execute(query)
-#             a = cursor.fetchall()
-#             b = {"temp": a[0][1]}
-#
-#             temp = json.dumps(b)
-#             yield f"data:{temp}\n\n"
-#             time.sleep(1)
-#     return Response(generator(), mimetype='text/event-stream')
+            cursor = conn.cursor()
+            query = ("select * from sensor_data order by date desc limit 1")
+            cursor.execute(query)
+            a = cursor.fetchall()
+            b = {"temp": a[0][1]}
+
+            temp = json.dumps(b)
+            yield f"data:{temp}\n\n"
+            time.sleep(1)
+    return Response(generator(), mimetype='text/event-stream')
 
 
 
